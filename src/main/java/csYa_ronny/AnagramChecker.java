321587e308b69b7e9126085b0606f18ee75d30bc
GuyArad1 <guyarad33@gmail.com>
2024-11-09 13:19:55 +0200

    HI

--- src/main/java/csYa_ronny/AnagramChecker.java
+++ src/main/java/csYa_ronny/AnagramChecker.java
@@ -3,7 +3,21 @@ package csYa_ronny;
 public class AnagramChecker {
     public static boolean areAnagrams(String str1, String str2) {
         // TODO: Implement the logic to check if str1 and str2 are anagrams
-        return false;  // Return false by default
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        int[] charCount = new int[256];
+        for (char c : str1.toCharArray()) {
+            charCount[c]++;
+        }
+        for (char c : str2.toCharArray()) {
+            charCount[c]--;
+            if (charCount[c] < 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
@@ -12,7 +26,6 @@ public class AnagramChecker {
         String word2 = "silent";
         String word3 = "apple";
         String word4 = "pale";
-
         System.out.println("Are \"" + word1 + "\" and \"" + word2 + "\" anagrams? " + areAnagrams(word1, word2));
         System.out.println("Are \"" + word3 + "\" and \"" + word4 + "\" anagrams? " + areAnagrams(word3, word4));
     }
